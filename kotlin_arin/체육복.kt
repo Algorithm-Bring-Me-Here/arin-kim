ce6955a2eda3c9d168ce205f1ca4795adefcf13b
arinming <jyandysys1@naver.com>
2024-05-13 15:00:21 +0900

    [week5] 체육복

---
+++ kotlin_arin/체육복.kt
@@ -0,0 +1,27 @@
+package kotlin_arin
+
+fun main() {
+    Solution().solution(5, intArrayOf(1,2), intArrayOf(3))
+}
+
+class Solution {
+    fun solution(n: Int, lost: IntArray, reserve: IntArray): Int {
+        var answer = 0
+
+        var lostList = (lost.sorted() - reserve.sorted()).toMutableList()
+        var reserveList = (reserve.sorted() - lost.sorted()).toMutableList()
+        answer = n - lostList.size
+
+        for (i in lostList) {
+            if (reserveList.contains(i - 1)) {
+                reserveList.remove(i - 1)
+                answer += 1
+            } else if (reserveList.contains(i + 1)) {
+                reserveList.remove(i + 1)
+                answer += 1
+            }
+        }
+
+        return answer
+    }
+}
